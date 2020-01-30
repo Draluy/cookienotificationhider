cbe2942887e3e1301e690da90d5f7cecf99e8293
draluy <draluy@yahoo.fr>
2020-01-30 21:34:11 +0100

    Added generic quantcast removal.

--- CookiesNotificationHider.js
+++ CookiesNotificationHider.js
@@ -11,6 +11,15 @@ let setIntervalX = function (callback, delay, repetitions) {
 let hidePeskyMessage = function () {
     let hostname = window.location.hostname
 
+    //first, find generic cookie notification providers.
+    // If found, remove the message and go no further
+    let quantcastMessage = document.querySelector(".qc-cmp-ui-container.qc-cmp-showing")
+    if (quantcastMessage) {
+        hideQuantcast()
+        return
+    }
+
+    // No generic message, search in site-specific database
     if (hideCookieMessage === null) {
         //nothing to do: we searched the database and failed to find a match
     } else if (hideCookieMessage !== undefined) {
